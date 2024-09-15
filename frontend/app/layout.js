5702c44c154c3f21702eb3750faffd324cc47ab6
Atharva SHandilya <atharvashandilya887@gmail.com>
2024-09-15 16:48:42 +0530

    added header and footer

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -19,11 +19,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en " className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#F2F2F2] min-h-full flex flex-col justify-between`}
       >
+        <header className="flex justify-between pt-6 px-[65px]">
+          <h1 className="font-bold text-xl text-[#282796]">DOCAID</h1>
+          <button className="bg-[#282796] text-white px-8 py-4 font-bold rounded-lg">
+            Logout
+          </button>
+        </header>
         {children}
+        <footer className="bg-[#282796] h-[178px] flex flex-col justify-end p-7">
+          <p className="text-white text-xl">All rights reserved by MedAura</p>
+        </footer>
       </body>
     </html>
   );
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,11 +1,12 @@
 "use client";
 
+import Card from "./component/Card";
 import OTP from "./component/OTP";
 
 export default function Home() {
   return (
     <main className=" ">
-      <OTP />
+      <Card />
     </main>
   );
 }
